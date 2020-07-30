1e2d7fad3158205f6e429b93c38ba3ba3b0661bd
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-30 14:14:18 -0700

    separating last5 and queryresults and displaying on the screen

--- assets/js/kelseyscript.js
+++ assets/js/kelseyscript.js
@@ -1,29 +1,29 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    //defaults to Seattle as the center
-    center: new google.maps.LatLng(47.6062, -122.3321),
-    mapTypeId: 'terrain'
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        //defaults to Seattle as the center
+        center: new google.maps.LatLng(47.6062, -122.3321),
+        mapTypeId: 'terrain'
+    });
 
 
-  var script = document.createElement('script');
-//placeholder for actual earthquake link
-  script.src = `http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp`;
-  document.getElementsByTagName('header')[0].appendChild(script);
+    var script = document.createElement('script');
+    //placeholder for actual earthquake link
+    script.src = `http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp`;
+    document.getElementsByTagName('header')[0].appendChild(script);
 }
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
 // need to get coordinates from Melinda
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-  }
+window.eqfeed_callback = function (results) {
+    for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+    }
 }
--- assets/js/melindascript.js
+++ assets/js/melindascript.js
@@ -1,16 +1,25 @@
-const lastHourQuakes = [];
-const quakesBySearch = [];
+let lastHourQuakes = [];
+let quakesBySearch = [];
 
 $(document).ready(
-    getdailyQuakes(),
-    // placeToCordinates("seattle")
+    getdailyQuakes()
 );
+
+$("#search").on("click", function (event) {
+    event.preventDefault();
+    const location = $("#location").val().trim();
+    const startDate = $("#startDate").val().trim();
+    const endDate = $("#endDate").val().trim();
+    const radius = $("#radius").val().trim();
+    placeToCordinates(location, startDate, endDate, radius);
+});
 
 function getdailyQuakes() {
     $.ajax({ url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson" })
-        .then(
-            (response) => collectData(response.features, lastHourQuakes)
-        );
+        .then(function (response) {
+            lastHourQuakes = collectData(response.features, lastHourQuakes),
+                renderPastHourQuakes(lastHourQuakes.slice(0, 5))
+        });
 }
 
 function collectData(features, array) {
@@ -22,19 +31,26 @@ function collectData(features, array) {
         const time = moment(element.properties.time).format("LLL");
         array.push({ place: place, coords: coords, mag: mag, time: time });
     }
-    renderPastHourQuakes(array.slice(0, 5));
+
+    return array;
+}
+
+function renderQuakesBySearch(array) {
+    for (let i = 0; i < array.length; i++) {
+        $($("#queryResults").append(createQuakesInfo(array[i], i, "searchQuakesResult", "resultelement")));
+    }
 }
 
 function renderPastHourQuakes(array) {
     for (let i = 0; i < array.length; i++) {
-        $($("#latestQs").append(createQuakesInfo(array[i], i)));
+        $($("#latestQs").append(createQuakesInfo(array[i], i, "latestQs", "latestPlace")));
     }
 }
 
-function createQuakesInfo(element, index) {
+function createQuakesInfo(element, index, id, classe) {
     return $("<p>")
-        .attr("id", `latest-${index}`)
-        .addClass("latest")
+        .attr("id", `${id}-${index}`)
+        .addClass(classe)
         .text(`Place: ${JSON.stringify(element.place)}, Magnitude: ${JSON.stringify(element.mag)}, Time: ${element.time}`);
 }
 
@@ -47,23 +63,18 @@ function getCoordinates(array) {
     return coords;
 }
 
-function placeToCordinates(place) {
+function placeToCordinates(place, startDate, endDate, radius) {
     const apiKey = '9bdca107dee44c8d90c4efabb9b500e4';
     $.ajax({ url: `https://api.opencagedata.com/geocode/v1/json?q=${place}&key=${apiKey}` })
         .then(
-            (response) => dataByLocation(response.results[0].geometry.lat, response.results[0].geometry.lng, quakesBySearch)
+            (response) => dataByLocation(response.results[0].geometry.lat, response.results[0].geometry.lng, quakesBySearch, startDate, endDate, radius)
         );
 }
 
-function dataByLocation(lat, lon, array) {
-    const radius = "100";
-    const startTime = "2020-07-01";
-    const endTime = "2020-07-27";
-    $.ajax({ url: `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startTime}&endtime=${endTime}&longitude=${lon}&latitude=${lat}&maxradiuskm=${radius}` })
-        .then(
-            (response) => collectData(response.features, array)
-        );
+function dataByLocation(lat, lon, array, startDate, endDate, radius) {
+    $.ajax({ url: `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startDate}&endtime=${endDate}&longitude=${lon}&latitude=${lat}&maxradiuskm=${radius}` })
+        .then(function (response) {
+            quakesBySearch = collectData(response.features, array),
+                renderQuakesBySearch(quakesBySearch)
+        });
 }
-
-console.log(lastHourQuakes)
-console.log(quakesBySearch)
