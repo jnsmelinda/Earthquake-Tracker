191df52e7b8384f1b7020fed1db2bf39be8cf64d
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-30 22:22:20 -0700

    Updating to add google maps

--- assets/js/kelseyscript.js
+++ assets/js/kelseyscript.js
@@ -1,48 +1,22 @@
-// line 4-13: data is accessable for kelseyscript.js:
-// 'event.detail' has the object array with the data and has to be passed to the function to be able to acces it.
-// to get the coordinates: event.detail[index].coords, printDailyQuakes() and printQuakesBySearch() are only for display
+//Gets information from Melindascript, so that it can be passed to the google maps API
 document.addEventListener('dailyQuakes', printDailyQuakes, false);
 document.addEventListener('quakesBySearch', printQuakesBySearch, false);
 
+
+//The below is to place coordinates for the quakes that occurred within the last hour
+//Map shows data to avoid blank spots, while the user figures out where they want to search. 
 function printDailyQuakes(event) {
-    console.log(event.detail);
-}
-
-function printQuakesBySearch(event) {
-    console.log(event.detail);
-}
-
-var map;
-var infowindow;
-var apiKey = '9bdca107dee44c8d90c4efabb9b500e4';
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: new google.maps.LatLng(47.6062, -122.3321),
-    mapTypeId: 'terrain'
-  });
-  infowindow = new google.maps.InfoWindow();
-  // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = `https://api.opencagedata.com/geocode/v1/json?q=${place}&key=${apiKey}`
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var text = '' + results.features[i].properties.place + '';
+  for (var i = 0; i < event.detail.length; i++) {
+    console.log(event.detail)
+    var coords = event.detail[i].coords;
+    var text = ' Location: ' + event.detail[i].place + ' Magnitude: ' + event.detail[i].mag + '' + ' Date: ' + event.detail[i].time + ' ';
     var latLng = new google.maps.LatLng(coords[1], coords[0]);
-
     var marker = new google.maps.Marker({
       position: latLng,
       map: map
     });
 
+//This adds the marker based on the last five quakes
     marker.addListener('click', (function(marker, text) {
       return function(e) {
         infowindow.setContent(text);
@@ -51,3 +25,40 @@ window.eqfeed_callback = function(results) {
     })(marker, text));
   }
 }
+
+//This is to get data for earthquakes that the user searches for rather than just the most recent quakes. 
+function printQuakesBySearch(event) {
+  for (var i = 0; i < event.detail.length; i++) {
+    console.log(event.detail.length)
+    var coords = event.detail[i].coords;
+    var text = '' + event.detail[i].place + '' ;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+//This adds the marker based on the user's search parameters
+    marker.addListener('click', (function(marker, text) {
+      return function(e) {
+        infowindow.setContent(text);
+        infowindow.open(map, marker);
+      }
+    })(marker, text));
+  }
+}
+
+//This sets the formatting for the map for either function, but centers the map on Seattle, where this specific class is located. 
+var map;
+var infowindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: new google.maps.LatLng(47.6062, -122.3321),
+    mapTypeId: 'terrain'
+    
+    
+  });
+  infowindow = new google.maps.InfoWindow();
+}
+
+
