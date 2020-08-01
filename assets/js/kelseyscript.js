e66410265fa779ef331b7ae1ec9e15dbc93b1764
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-08-01 11:00:42 -0700

    Updated to add more descriptive text to the pin markers

--- assets/js/kelseyscript.js
+++ assets/js/kelseyscript.js
@@ -4,12 +4,12 @@ document.addEventListener('quakesBySearch', printQuakesBySearch, false);
 
 
 //The below is to place coordinates for the quakes that occurred within the last hour
-//Map shows data to avoid blank spots, while the user figures out where they want to search. 
+//Map shows data to avoid blank spots, while the user figures out where they want to search.
 function printDailyQuakes(event) {
   for (var i = 0; i < event.detail.length; i++) {
     console.log(event.detail)
     var coords = event.detail[i].coords;
-    var text = ' Location: ' + event.detail[i].place + ' Magnitude: ' + event.detail[i].mag + '' + ' Date: ' + event.detail[i].time + ' ';
+    var text = '5 Most Recent Rumbles - Location: ' + event.detail[i].place + ' Magnitude: ' + event.detail[i].mag + '' + ' Date: ' + event.detail[i].time + ' ';
     var latLng = new google.maps.LatLng(coords[1], coords[0]);
     var marker = new google.maps.Marker({
       position: latLng,
@@ -26,12 +26,12 @@ function printDailyQuakes(event) {
   }
 }
 
-//This is to get data for earthquakes that the user searches for rather than just the most recent quakes. 
+//This is to get data for earthquakes that the user searches for rather than just the most recent quakes.
 function printQuakesBySearch(event) {
   for (var i = 0; i < event.detail.length; i++) {
     console.log(event.detail.length)
     var coords = event.detail[i].coords;
-    var text = '' + event.detail[i].place + '' ;
+    var text = 'Searched Rumbles - Location: ' + event.detail[i].place + ' Magnitude: ' + event.detail[i].mag + '' + ' Date: ' + event.detail[i].time + ' ';
     var latLng = new google.maps.LatLng(coords[1], coords[0]);
     var marker = new google.maps.Marker({
       position: latLng,
@@ -47,7 +47,7 @@ function printQuakesBySearch(event) {
   }
 }
 
-//This sets the formatting for the map for either function, but centers the map on Seattle, where this specific class is located. 
+//This sets the formatting for the map for either function, but centers the map on Seattle, where this specific class is located.
 var map;
 var infowindow;
 function initMap() {
@@ -55,8 +55,8 @@ function initMap() {
     zoom: 3,
     center: new google.maps.LatLng(47.6062, -122.3321),
     mapTypeId: 'terrain'
-    
-    
+
+
   });
   infowindow = new google.maps.InfoWindow();
 }
